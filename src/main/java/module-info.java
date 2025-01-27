700a5bcc0ab90a293beaf2dad1016b7746f812dd
marcus <marcus.viden@ub.uu.se>
2025-01-27 12:22:45 +0000

    Disable urn extended functionality until CORA-2781 is Done.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,8 @@
-import se.uu.ub.cora.diva.extended.divaoutput.urn.UrnExtendedFunctionalityFactory;
-import se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionalityFactory;
-
 module se.uu.ub.cora.diva {
 	requires se.uu.ub.cora.spider;
 	requires se.uu.ub.cora.data;
 	requires se.uu.ub.cora.storage;
 
-	provides ExtendedFunctionalityFactory with UrnExtendedFunctionalityFactory;
+	// provides ExtendedFunctionalityFactory with UrnExtendedFunctionalityFactory;
 
 }
